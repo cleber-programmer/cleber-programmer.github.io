ab44af8893a052b9f0a62e2ba0085b6a39e3c45f
Cleber de Moraes Goncalves <cleber.programmer@hotmail.com>
2016-05-30 20:15:07 +0000

    novo arquivo final

--- app/interceptor/token.js
+++
@@ -1,9 +0,0 @@
-Rex('app.interceptor.token', ['add'], function (add) {
-  
-  return {
-    request: function (method, url, data, xhr) {
-      return xhr.setRequestHeader('Authorization', add('Bearer ', '363102d7f9470edb0ef1464915fb8c3c92519756164ed476b2a560cbf85df48a')), data;
-    }
-  };
-  
-});
