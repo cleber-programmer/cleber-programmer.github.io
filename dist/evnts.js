88be7de95cd1f93a9ea40650b40b6fc827e757f1
cleber.programmer <cleber.programmer@hotmail.com>
2017-06-11 10:50:35 -0300

    Evnts | Hotel

--- dist/evnts.js
+++ dist/evnts.js
@@ -4,9 +4,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
@@ -14,6 +11,9 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 (function (modules, proxy) {
     Object.assign(window, {
@@ -626,19 +626,48 @@ Rex(function (R) {
 Rex('partial', (R) => (target, ...a) => (...b) => target(...R.concat(a, b)));
 Rex(function (R) {
     let Perfil = class Perfil {
-        selectThumbnail({ target: { files: [thumbnail] } }) {
-            return __awaiter(this, void 0, void 0, function* () {
-                R.sheet('.form_thumbnail:after', ['background-image', `url(${(yield R.crop(thumbnail)).target.result})`]);
-            });
+    };
+    Perfil = __decorate([
+        R.atom.component('form-perfil', 'form')
+    ], Perfil);
+});
+Rex('thumbnailPreview', function (R) {
+    return function ({ target: { files: [thumbnail] } }) {
+        return __awaiter(this, void 0, void 0, function* () {
+            R.sheet('.form_thumbnail:after', ['background-image', `url(${(yield R.crop(thumbnail)).target.result})`]);
+        });
+    };
+});
+Rex('thumbnailTemplate', function () {
+    return function () {
+        return (`
+      <input type="file" class="form_thumbnail">
+    `);
+    };
+});
+Rex(function (R) {
+    let Thumbnail = class Thumbnail {
+        render() {
+            return R.thumbnailTemplate();
+        }
+        selectImage(event) {
+            R.thumbnailPreview(event);
         }
     };
+    __decorate([
+        R.atom.attached,
+        R.atom.innerHTML,
+        __metadata("design:type", Function),
+        __metadata("design:paramtypes", []),
+        __metadata("design:returntype", void 0)
+    ], Thumbnail.prototype, "render", null);
     __decorate([
         R.atom.event('change', '.form_thumbnail'),
         __metadata("design:type", Function),
         __metadata("design:paramtypes", [Object]),
-        __metadata("design:returntype", Promise)
-    ], Perfil.prototype, "selectThumbnail", null);
-    Perfil = __decorate([
-        R.atom.component('form-perfil', 'form')
-    ], Perfil);
+        __metadata("design:returntype", void 0)
+    ], Thumbnail.prototype, "selectImage", null);
+    Thumbnail = __decorate([
+        R.atom.component('evnts-thumbnail')
+    ], Thumbnail);
 });
